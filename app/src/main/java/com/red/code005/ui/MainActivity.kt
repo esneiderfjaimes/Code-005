3ffa09b6496867ea576ec153b57947226568cc88
Esneider Jaimes <62038623+esneiderfjaimes@users.noreply.github.com>
2022-01-19 23:08:40 -0500

    permission request implemented
    • added request for required permissions
    • added Login view (Autogenerated by Android Studio) as temporary view
    • Material 3 is set by base on the themes of the application

---
+++ app/src/main/java/com/red/code005/ui/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.red.code005.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.red.code005.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+    }
+}
---
+++ app/src/main/java/com/red/code005/ui/PermissionsFragment.kt
@@ -0,0 +1,79 @@
+package com.red.code005.ui
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.Navigation
+import com.red.code005.R
+
+private val PERMISSIONS_REQUIRED = arrayOf(
+    Manifest.permission.CAMERA,
+    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+    Manifest.permission.READ_EXTERNAL_STORAGE
+)
+
+/**
+ * The sole purpose of this fragment is to request permissions and, once granted,
+ * display the login fragment to the user.
+ */
+class PermissionsFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // check if the necessary permissions exist
+        if (hasPermissions(requireContext(), *PERMISSIONS_REQUIRED)) {
+            Log.i(
+                "PermissionsLogs",
+                "onCreate: nice! go to login"
+            )
+            navigateToLogin()
+            return
+        }
+
+        // if the permissions do not exist, start the request
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { map ->
+            var result = true
+            map.entries.forEach { entry ->
+                if (!entry.value) {
+                    Log.w(
+                        "PermissionsLogs",
+                        "onCreate: oh no!, permission ${entry.key} has been denied"
+                    )
+                    result = false
+                    return@forEach
+                }
+            }
+            if (result) navigateToLogin()
+            // TODO: what to show when denying permissions?
+            else activity?.finish()
+        }.launch(PERMISSIONS_REQUIRED)
+    }
+
+    private fun navigateToLogin() {
+        lifecycleScope.launchWhenStarted {
+            Navigation.findNavController(requireActivity(), R.id.fragment_container)
+                .navigate(PermissionsFragmentDirections.actionPermissionsToLogin())
+        }
+    }
+
+    companion object {
+        /**
+         * Convenience method used to check if all permissions required by this app are granted
+         */
+        fun hasPermissions(context: Context) = PERMISSIONS_REQUIRED.all {
+            ContextCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_DENIED
+        }
+
+        fun hasPermissions(context: Context, vararg permissions: String): Boolean = permissions.all {
+            ActivityCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+}
